b8b708f3cd039f301fe45e27d5203658a668530a
DannaH19 <dlherrerar@itc.edu.co>
2025-02-27 18:29:03 +0000

    feat: se crea el menu y se actualiza el codigo fuente

---
+++ my-vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    <a href="" target="_blank"
+      >BIENVENIDOS</a
+    >, 
+  </p>
+  <p>
+     Más Información
+    <a
+      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
+      target="_blank"
+      ></a
+    >
+  </p>
+  <p class="read-the-docs">Sigueme</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ my-vue-app/src/components/Menu.vue
@@ -0,0 +1,48 @@
+<!--este seccion de los template sirve para todo el codigo html-->
+<template>
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    
+    <a class="navbar-brand" href="https://www.google.com/">Proyecto DH</a>
+    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">inicio</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="#">contactos</a>
+        </li>      
+        <li class="nav-item">
+          <a class="nav-link" href="#">productos</a>
+        </li>    
+    </ul>
+    </div>
+  </div>
+</nav>
+
+</template>
+
+
+
+
+<!--este seccion de los script sirve para logica y es neto js-->
+<script>
+
+
+</script>
+
+
+
+
+<!----> sirve para todo lo relacionado del diseño y se coloca scope solo servira en el componente-->
+<style scope>
+
+
+</style>
