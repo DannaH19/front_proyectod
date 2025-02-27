9565a226b2dcfd14616b317484dd082387adeab2
DannaH19 <dlherrerar@itc.edu.co>
2025-02-27 23:07:53 +0000

    fix: creamos un formulario en contenedor

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import MenuPrincipal from './components/Menu.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+//import MenuPrincipal from './components/Menu.vue'
+import ContenedorEjemplo from './components/contenedor.vue'
+
 </script>
 
 <template>
-  <div class="container-fluid">
+ <!--<div class="container-fluid">
   <MenuPrincipal/>
   <HelloWorld/>
 </div>
+-->
+<ContenedorEjemplo>
+</ContenedorEjemplo>
+
 </template>
 
 <style scoped>
---
+++ src/components/contenedor.vue
@@ -0,0 +1,43 @@
+<template>
+ <div class="mi_primer_diseño">
+    <p>este es mi primer parrafo: jkajldhadlk</p>
+     </div>
+     <div id="from" class="container">
+        <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">Nombre y apellido</label>
+       <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Coloque su nombre">
+      </div>
+     <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Cedula</label>
+        <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="Coloque su nombre">
+      </div>
+      <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+     </div>
+
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+.mi_primer_diseño{
+    background-color: rgb(43, 226, 211);
+    width: 400px;
+    height: 300px;
+    position: relative;
+    padding: 14px;
+    margin: 4px;
+    top: 50%;
+    left: 50%;
+}
+</style>
