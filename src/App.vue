cbabca1472c9fbf580321d693172e36bd3bf7b16
DannaH19 <dlherrerar@itc.edu.co>
2025-03-14 01:47:36 +0000

    fix: se guardo el codigo

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <script setup>
-//import HelloWorld from './components/HelloWorld.vue'
-//import MenuPrincipal from './components/Menu.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import MenuPrincipal from './components/Menu.vue'
 import ContenedorEjemplo from './components/contenedor.vue'
 
 </script>
 
 <template>
- <!--<div class="container-fluid">
+ <div class="container-fluid">
   <MenuPrincipal/>
+
+  <ContenedorEjemplo>
+</ContenedorEjemplo>
+
+
+
   <HelloWorld/>
 </div>
--->
-<ContenedorEjemplo>
-</ContenedorEjemplo>
+
+
 
 </template>
 
--- src/components/contenedor.vue
+++ src/components/contenedor.vue
@@ -1,25 +1,19 @@
 <template>
- <div class="mi_primer_diseño">
-    <p>este es mi primer parrafo: jkajldhadlk</p>
-     </div>
-     <div id="from" class="container">
-        <div class="mb-3">
-      <label for="exampleFormControlInput1" class="form-label">Nombre y apellido</label>
-       <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Coloque su nombre">
+
+  <div id="from" class="container">
+    <div class="mb-3 row">
+      <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+      <div class="col-sm-10">
+        <input type="text"  class="form-control" id="staticEmail" value="">
       </div>
-     <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Cedula</label>
-        <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="Coloque su nombre">
+    </div>
+    <div class="mb-3 row">
+      <label for="inputPassword" class="col-sm-2 col-form-label">Contraseña</label>
+      <div class="col-sm-10">
+        <input type="password" class="form-control" id="inputPassword">
       </div>
-      <div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Email</label>
-  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-</div>
-<div class="mb-3">
-  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-</div>
-     </div>
+    </div>
+  </div>
 
 
 </template>
@@ -28,16 +22,4 @@
 
 </script>
 
-<style scoped>
-
-.mi_primer_diseño{
-    background-color: rgb(43, 226, 211);
-    width: 400px;
-    height: 300px;
-    position: relative;
-    padding: 14px;
-    margin: 4px;
-    top: 50%;
-    left: 50%;
-}
-</style>
+<style scoped></style>
